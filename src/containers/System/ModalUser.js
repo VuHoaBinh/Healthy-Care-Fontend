c7f7bc286bab1605f584354759eff73ff1ee2c67
Vũ Hòa Bình <89285687+VuHoaBinh@users.noreply.github.com>
2023-10-01 17:52:15 +0700

    Update ModalUser.js

--- src/containers/System/ModalUser.js
+++ src/containers/System/ModalUser.js
@@ -7,12 +7,99 @@ import "./UserManage.scss";
 class ModalUser extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      email: "",
+      password: "",
+      firstName: "",
+      lastName: "",
+      address: "",
+      phone: "",
+    };
   }
+  // const [gender, setGender] = useState('male');
+
+  // const handleGenderChange = (e) => {
+  //   setGender(e.target.value);
+  // };
   componentDidMount() {}
   toggle = () => {
     this.props.toggleUserModal();
   };
+
+  handleOnChangeALLinput = (event, id) => {
+    // this.state[id] = event.target.value;
+    // this.setState({
+    //   ...this.state,
+    // });
+    // console.log(this.state);
+
+    let copyState = { ...this.state };
+    copyState[id] = event.target.value;
+    this.setState({
+      ...copyState,
+    });
+    // console.log(event.target.value, id);
+  };
+
+  isCheckValid = () => {
+    let isValid = true;
+    let arrInput = [
+      "email",
+      "password",
+      "firstName",
+      "lastName",
+      "address",
+      "phone",
+    ];
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const namePattern = /^[A-Za-z]+([-']?[A-Za-z]+)*$/;
+    const addressPattern = /^[A-Za-z0-9\s.,#-]+$/;
+    const phonePattern = /^\d{11}$/;
+    for (let i = 0; i < arrInput.length; i++) {
+      if (!this.state[arrInput[i]]) {
+        isValid = false;
+        alert("Please enter a valid" + arrInput[i]);
+        break;
+      }
+      // if (arrInput[i] === "email" && !emailPattern.test(this.state.email)) {
+      //   isValid = false;
+      //   alert("Please enter your Email");
+      //   break;
+      // }
+      // if (
+      //   (arrInput[i] === "firstName" || arrInput[i] === "lastName") &&
+      //   (!namePattern.test(this.state.firstName) ||
+      //     !namePattern.test(this.state.lastName))
+      // ) {
+      //   isValid = false;
+      //   alert("Please enter your Name");
+      //   break;
+      // }
+      // if (
+      //   arrInput[i] === "address" &&
+      //   !addressPattern.test(this.state.address)
+      // ) {
+      //   isValid = false;
+      //   alert("Please enter your Address");
+      //   break;
+      // }
+      // if (arrInput[i] === "phone" && !phonePattern.test(this.state.phone)) {
+      //   isValid = false;
+      //   alert("Please enter your Address");
+      //   break;
+      // }
+    }
+    return isValid;
+  };
+  handleOnClickAddNew = () => {
+    let isValid = this.isCheckValid();
+    if (isValid) {
+      console.log("state: ", this.state);
+      // call API
+      this.props.getCreateUserModal(this.state);
+    }
+  };
+
   render() {
     return (
       <Modal
@@ -32,39 +119,90 @@ class ModalUser extends Component {
         <ModalBody>
           <div className="modal-user-body">
             <div className="input-container">
-              <label for="name">Email:</label>
-              <input type="text" className="form-control" />
+              <label htmlFor="Email">Email:</label>
+              <input
+                type="text"
+                className="form-control"
+                onChange={(event) => {
+                  this.handleOnChangeALLinput(event, "email");
+                }}
+                value={this.state.email}
+                placeholder="hoabinh.vippro63@gmail.com"
+              />
             </div>
             <div className="input-container">
-              <label for="name">Password:</label>
-              <input type="password" className="form-control" />
+              <label htmlFor="Password">Password:</label>
+              <input
+                type="password"
+                className="form-control"
+                onChange={(event) => {
+                  this.handleOnChangeALLinput(event, "password");
+                }}
+                value={this.state.password}
+              />
             </div>
             <div className="input-container">
-              <label for="name">FirstName:</label>
-              <input type="text" className="form-control" />
+              <label htmlFor="FirstName">FirstName:</label>
+              <input
+                type="text"
+                className="form-control"
+                onChange={(event) => {
+                  this.handleOnChangeALLinput(event, "firstName");
+                }}
+                value={this.state.firstName}
+                placeholder="Vu"
+              />
             </div>
             <div className="input-container">
-              <label for="name">LastName:</label>
-              <input type="password" className="form-control" />
+              <label htmlFor="LastName">LastName:</label>
+              <input
+                type="text"
+                className="form-control"
+                onChange={(event) => {
+                  this.handleOnChangeALLinput(event, "lastName");
+                }}
+                value={this.state.lastName}
+                placeholder="Hoa Binh"
+              />
             </div>
             <div className="input-container max-width-input">
-              <label for="name">Address:</label>
-              <input type="password" className="form-control" />
+              <label htmlFor="Address">Address:</label>
+              <input
+                type="text"
+                className="form-control"
+                onChange={(event) => {
+                  this.handleOnChangeALLinput(event, "address");
+                }}
+                value={this.state.address}
+                placeholder="90 Khue My"
+              />
+            </div>
+            <div className="input-container max-width-input">
+              <label htmlFor="Phone">Phone:</label>
+              <input
+                type="text"
+                className="form-control"
+                onChange={(event) => {
+                  this.handleOnChangeALLinput(event, "phone");
+                }}
+                value={this.state.phone}
+                placeholder="12312312389"
+              />
             </div>
           </div>
         </ModalBody>
         <ModalFooter>
           <Button
-            color="secondary"
+            color="primary"
             className="px-3"
             onClick={() => {
-              this.toggle();
+              this.handleOnClickAddNew();
             }}
           >
             Save
           </Button>
           <Button
-            color="primary"
+            color="secondary"
             className="px-3"
             onClick={() => {
               this.toggle();
