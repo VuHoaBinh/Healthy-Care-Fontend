5c51b2625ab0d5e2950d7ddd65279b143535ce73
Vũ Hòa Bình <89285687+VuHoaBinh@users.noreply.github.com>
2023-10-01 17:52:42 +0700

    Update userServices.js

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -7,4 +7,8 @@ const handleLoginAPI = (email, password) => {
 const getAllUser = (ID) => {
   return axios.get(`/api/getAllUsers?id=${ID}`);
 };
-export { handleLoginAPI, getAllUser };
+
+const getCreateUserService = (data) => {
+  return axios.post("/api/create-new-user", data);
+};
+export { handleLoginAPI, getAllUser, getCreateUserService };
